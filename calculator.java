64db8be9334b36a4bf1ef388b10b41a6ac870db2
Emanuel Berger <94300384+emaa10@users.noreply.github.com>
2023-03-01 11:37:17 +0100

    add calculator

---
+++ calculator.java
@@ -0,0 +1,26 @@
+/*
+* Art
+* 1 → Addieren
+* 2 → Subtrahieren
+* 3 → Dividieren
+* 4 → Multiplizieren
+*/
+
+
+
+int calculate(int art, double zahl1, double zahl2) {
+    double result;
+    if(art == 1) {
+        result = zahl1 + zahl2;
+    } else if(art == 2) {
+        result = zahl1 - zahl2;
+    } else if(art == 3) {
+        result = zahl1 / zahl2;
+    } else if(art == 4) {
+        result = zahl1 * zahl2;
+    }
+    return result;
+}
+
+//Example:
+System.out.println(calculate(4, 4, 2)); //Multipliziert 4*2
